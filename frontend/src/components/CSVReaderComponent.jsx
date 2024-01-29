e75c99b5462facbba678f569f25e7e19ed88bf02
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2024-01-29 18:02:29 +0200

    map satelite and button import fix

--- frontend/src/components/CSVReaderComponent.jsx
+++ frontend/src/components/CSVReaderComponent.jsx
@@ -1,23 +1,36 @@
 // CSVReaderComponent.js
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import CSVReader from 'react-csv-reader';
 import axios from 'axios';
+import '../custom.css';
 
 const CSVReaderComponent = () => {
   const handleFile = (data, fileInfo) => {
     // Send data to Node.js server
-    console.log(data)
-    axios.post('http://localhost:5000/upload-csv', { data })
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
+
+    // Show alert before sending data
+    // Show confirmation dialog before sending data
+    const userConfirmed = window.confirm('Are you sure you want to upload this data?');
+
+    if (userConfirmed) {
+        console.log(data)
+        axios.post('http://localhost:5000/upload-csv', { data })
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    }else{
+    // User clicked "Cancel," do nothing or handle as needed
+    console.log('User canceled upload.');
+    }
   };
 
+
   return (
-    <CSVReader onFileLoaded={handleFile} cssClass="csv-reader-input" label="Import CSV"  />
+    <CSVReader onFileLoaded={handleFile}    cssClass="csv-reader-input" />
+    // inputStyle={   background-color: '#00d1b2';border-color: 'transparent';color: '#fff';}
   );
 };
 
