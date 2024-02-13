b500e590ce9558a505e750ec5f19cac5a6b3dd50
A-PALIOS <paliosadonis6@gmail.com>
2024-02-13 17:53:47 +0200

    db-online

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db=new Sequelize('auth_db','root','',{
-    host:"localhost",
+const db=new Sequelize('environ_db','environ_user','#eUq.x43s[;&',{
+    host:"5.77.39.26",
     dialect:"mysql",
     port:3306
 });
